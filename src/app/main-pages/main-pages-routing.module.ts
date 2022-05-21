ef2ca6bed0ac24adb7fd2875487db44f5df93175
GreenBeret3155 <hoangmanhhung99@gmail.com>
2022-05-21 17:47:23 +0700

    header1

--- src/app/main-pages/main-pages-routing.module.ts
+++ src/app/main-pages/main-pages-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from '../pages/miscellaneous/not-found/not-found.component';
+import { DownloadPageComponent } from '../shared/components/download-page/download-page.component';
 import { MainPagesComponent } from './main-pages.component';
 
 
@@ -8,11 +10,34 @@ const routes: Routes = [{
   component: MainPagesComponent,
   children: [
     {
+      path: 'user-management',
+      loadChildren: () => import('../pages/user-management/user-management.module')
+        .then(m => m.UserManagementModule)
+    },
+    {
+      path: 'chat-offline',
+      loadChildren: () => import('../chat-custom/chat-custom.module')
+        .then(m => m.ChatCustomModule),
+      data: {
+        breadcrumb: {
+          label: 'Chat offline',
+        },
+      }
+    },
+    {
+      path: 'download-file/:link',
+      component: DownloadPageComponent,
+    },
+    {
       path: '',
       redirectTo: 'user-management',
       pathMatch: 'full',
     },
-  ],
+    {
+      path: '**',
+      component: NotFoundComponent,
+    },
+  ]
 }];
 
 @NgModule({
